2db41758798c0325e6ad2bf9ee158c2fe37955c5
Stefan Nožinić <stefan@lugons.org>
2024-06-27 21:08:39 +0200

    Add training function to train.py

--- crypto_guard/train.py
+++ crypto_guard/train.py
@@ -1,2 +1,11 @@
-def train(input: str):
-    return 
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
+from crypto_guard.model import *
+def train(spark, input: str, model_path: str):
+    df = spark.read.csv(input, header=True, inferSchema=True)
+    pipeline, evaluator, cv = get_model()
+    model = cv.fit(df)
+    print(f"Best model: {model.bestModel}")
+    print(f"Best model params: {model.bestModel.stages[-1].extractParamMap()}")
+    print(f"Best model evaluator: {model.avgMetrics}")
+    model.save(model_path)
